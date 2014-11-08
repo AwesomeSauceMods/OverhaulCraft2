6a9f7944e6be9ee2898e3feffbb0d6db2eb8b996
gjgfuj <gjgfujb@gmail.com>
2014-11-08 21:17:28 +1100

    Dirty Hackering.

---
+++ main/java/com/awesomesauce/minecraft/forge/overhaul2/ItemOverhaulAxe.java
@@ -0,0 +1,11 @@
+package com.awesomesauce.minecraft.forge.overhaul2;
+
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemAxe;
+
+class ItemOverhaulAxe extends ItemAxe {
+    public ItemOverhaulAxe(Item.ToolMaterial material) {
+        super(material);
+    }
+}
--- main/scala/com/awesomesauce/minecraft/forge/overhaul2/ItemOverhaulAxe.scala
+++
@@ -1,8 +0,0 @@
-package com.awesomesauce.minecraft.forge.overhaul2
-
-import net.minecraft.item.{Item, ItemAxe}
-
-
-class ItemOverhaulAxe(material: Item.ToolMaterial) extends ItemAxe(material) {
-
-}
